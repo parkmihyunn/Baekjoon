781d3246e3f8670fa8784ece561d7f497bc4e92e
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2024-09-30 00:40:12 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.17 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,8 +1,6 @@
-const solution = nums  => {
-    let count = [0,0];
-    for(let i=0; i<nums.length; i++){
-        if(nums[i]%2 === 0) count[0] += 1;
-        else count[1] += 1;
-    }
-    return count;
+const solution = nums => {
+    return [
+        nums.filter((num) => num % 2 === 0).length,
+        nums.filter((num) => num % 2 === 1).length
+    ];
 };
