273b3dc9970ecd81cc13a6829b955c81bfbc71b2
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-03-30 00:12:39 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 27.70 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    return arr.map(item => {
+        if(item >= 50 && item % 2 === 0) return item/2;
+        if(item < 50 && item % 2 !== 0) return item*2;
+        else return item;
+        }
+    )
+}
