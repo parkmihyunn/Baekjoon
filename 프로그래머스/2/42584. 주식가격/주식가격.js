420fc53849e5f7ade9416485dbfb36357cb35cfd
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-05-23 17:53:22 +0900

    [level 2] Title: 주식가격, Time: 6.99 ms, Memory: 45.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,13 +1,28 @@
+// function solution(prices) {
+//     let anw = [];
+//     prices.forEach((price, idx) => {
+//         let temp = price;
+//         let time = 0;
+//         for(let i=idx+1; i<prices.length; i++) {
+//             time += 1;
+//             if(temp > prices[i]) break;
+//         }
+//         anw.push(time);
+//     })
+//     return anw;
+// }
+
 function solution(prices) {
-    let anw = [];
-    prices.forEach((price, idx) => {
-        let temp = price;
-        let time = 0;
-        for(let i=idx+1; i<prices.length; i++) {
-            time += 1;
-            if(temp > prices[i]) break;
+    const anw = [];
+    const reversePrices = prices.reverse();
+    while(reversePrices.length) {
+        const price = reversePrices.pop();
+        let cnt = 0;
+        for (let i = reversePrices.length-1 ; i >= 0 ; i--) {
+            cnt += 1
+            if (price > reversePrices[i]) break;
         }
-        anw.push(time);
-    })
+        anw.push(cnt);
+    }
     return anw;
 }
