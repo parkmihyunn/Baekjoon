bdee085c7bf87db06f5506b2bdbfc29749ff37d6
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-05-26 17:57:06 +0900

    [level 3] Title: 110 옮기기, Time: 173.97 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.js
@@ -0,0 +1,21 @@
+function solution(s) {
+    let anw = s.map((numbers) => {
+        const stack = [];
+        let str = "";
+        for (const char of numbers) {
+            if(stack.length >= 2) {
+                const mid = stack.pop();
+                const left = stack.pop();
+                `${left}${mid}${char}` === "110" ? (str += "110") : stack.push(left, mid, char);
+            } else {
+                stack.push(char);
+            }
+        }
+        const stackStr = stack.join("");
+        const index = stackStr.lastIndexOf("0");
+        return index === -1 
+                    ? str + stackStr
+                    : stackStr.slice(0, index+1) + str + stackStr.slice(index+1);
+    });
+    return anw;
+}
