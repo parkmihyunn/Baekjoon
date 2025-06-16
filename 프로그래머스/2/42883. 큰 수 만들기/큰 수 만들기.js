7a4369f84fb833ef7b130ee33779dcc9f7c800c2
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-06-16 18:10:24 +0900

    [level 2] Title: 큰 수 만들기, Time: 33.65 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,11 @@
+function solution(number, k) {
+    const answer = [];
+    for(let num of number){
+        while(k>0 && answer[answer.length-1] < num){
+            answer.pop(num);
+            k-=1;
+        }
+        answer.push(num);
+    }
+    return answer.join("").slice(0, number.length-k);
+}
