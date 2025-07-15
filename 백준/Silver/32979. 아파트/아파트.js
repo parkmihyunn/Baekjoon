7adfa93d83606a03fadcf44c8d94d4dc25bc32b4
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-07-15 20:15:15 +0900

    [Silver V] Title: 아파트, Time: 632 ms, Memory: 13464 KB -BaekjoonHub

--- 백준/Silver/32979. 아파트/아파트.js
+++ 백준/Silver/32979. 아파트/아파트.js
@@ -4,15 +4,12 @@ const input = fs
   .toString()
   .trim()
   .split("\n");
-
 const hands = input[2].split(" ").map(Number);
 const picked = input[3].split(" ").map(Number);
 
-let time = 0;
-while (time !== Number(input[1])) {
-  for (let i = 0; i < picked[time] - 1; i++) {
+for (let t = 0; t < Number(input[1]); t++) {
+  for (let i = 0; i < picked[t] - 1; i++) {
     hands.push(hands.shift());
   }
   console.log(hands[0]);
-  time += 1;
 }
