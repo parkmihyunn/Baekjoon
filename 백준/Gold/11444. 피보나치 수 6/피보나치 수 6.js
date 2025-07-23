12b9b3236eb1353880557dcacc894afa1f0f94fd
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-07-23 17:11:24 +0900

    [Gold II] Title: 피보나치 수 6, Time: 108 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const N = BigInt(fs.readFileSync("dev/stdin").toString().trim());
+const MOD = 1000000007n;
+
+function mul(A, B) {
+  return [
+    [
+      (A[0][0] * B[0][0] + A[0][1] * B[1][0]) % MOD,
+      (A[0][0] * B[0][1] + A[0][1] * B[1][1]) % MOD,
+    ],
+    [
+      (A[1][0] * B[0][0] + A[1][1] * B[1][0]) % MOD,
+      (A[1][0] * B[0][1] + A[1][1] * B[1][1]) % MOD,
+    ],
+  ];
+}
+
+function power(matrix, n) {
+  let result = [
+    [1n, 0n],
+    [0n, 1n],
+  ];
+  while (n > 0n) {
+    if (n & 1n) result = mul(result, matrix);
+    matrix = mul(matrix, matrix);
+    n >>= 1n;
+  }
+  return result;
+}
+
+if (N === 0n) {
+  console.log(0);
+} else {
+  const base = [
+    [1n, 1n],
+    [1n, 0n],
+  ];
+  const result = power(base, N - 1n);
+  console.log(Number(result[0][0] % MOD));
+}
