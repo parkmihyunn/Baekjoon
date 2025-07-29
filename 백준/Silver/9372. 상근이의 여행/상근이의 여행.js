231248f7098e4102bb77d1d26884330d918437a5
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-07-29 15:58:28 +0900

    [Silver IV] Title: 상근이의 여행, Time: 124 ms, Memory: 20524 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.js
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.js
@@ -1,37 +1,13 @@
 const fs = require("fs");
 const input = fs.readFileSync("dev/stdin", "utf8").trim().split("\n");
-const T = +input[0];
+let idx = 0;
+const T = +input[idx++];
 let anw = [];
-let temp = 0;
-for (let i = 0; i < T; i++) {
-  const [N, M] = input[1 + temp].split(" ").map(Number);
-  const routes = input
-    .slice(2 + temp, 2 + temp + M)
-    .map((el) => el.split(" ").map((el) => Number(el) - 1));
-  temp += M + 1;
 
-  let count = 0;
-  const countries = new Array(N).fill(false);
-
-  countries[0] = true;
-  const queue = [0];
-
-  while (queue.length) {
-    const shifted = queue.shift();
-    for (let route of routes) {
-      const [start, end] = route;
-      if (start === shifted && !countries[end]) {
-        queue.push(end);
-        countries[end] = true;
-        count++;
-      } else if (end === shifted && !countries[start]) {
-        queue.push(start);
-        countries[start] = true;
-        count++;
-      }
-    }
-    if (!countries.includes(false)) break;
-  }
-  anw[i] = count;
+for (let t = 0; t < T; t++) {
+  const [N, M] = input[idx++].split(" ").map(Number);
+  idx += M;
+  anw.push(N - 1);
 }
+
 console.log(anw.join("\n"));
