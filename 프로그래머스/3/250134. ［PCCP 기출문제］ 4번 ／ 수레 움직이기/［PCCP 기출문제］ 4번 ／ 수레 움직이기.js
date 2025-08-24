2f3dfdee956fda8771ab8b049957216d0f273737
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2025-08-24 15:54:08 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 30.34 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.js
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.js
@@ -1,68 +1,64 @@
 function solution(maze) {
-    let anw = 0;
     const N = maze.length;
     const M = maze[0].length;
     
-//     let nowRX = 0, nowRY = 0, nowBX = 0, nowBY = 0;
+    let nowRX = 0, nowRY = 0, nowBX = 0, nowBY = 0;
+    let targetRX = 0, targetRY = 0, targetBX = 0, targetBY = 0;
     let nowR = [], nowB = [];
     let targetR = [], targetB = [];
     maze.forEach((el, i) => {
         el.forEach((item, j) => {
-            if(item === 1) nowR = [i, j]; 
-            if(item === 2) nowB = [i, j];
-            if(item === 3) targetR = [i, j];
-            if(item === 4) targetB = [i, j];
+            if(item === 1) nowRX = j, nowRY = i;
+            if(item === 2) nowBX = j, nowBY = i;
+            if(item === 3) targetRX = j, targetRY = i;
+            if(item === 4) targetBX = j, targetBY = i;
         })
     });
-    const [rgy, rgx] = targetR, [bgy, bgx] = targetB;
-    const [rsy, rsx] = nowR,   [bsy, bsx] = nowB;
     
+    const rSet = new Set([`${nowRY}, ${nowRX}`]);
+    const bSet = new Set([`${nowBY}, ${nowBX}`]);
+    const queue = [[nowRX, nowRY, nowBX, nowBY, rSet, bSet, 0]];
+
+    const visited = new Set();
+    const setToKey = (set) => [...set].sort().join('+');
+    visited.add(`${nowRY},${nowRX}+${nowBY},${nowBX}+${setToKey(rSet)}+${setToKey(bSet)}`);
+    
+    const checkInside = (y, x) => y >= 0 && y < N && x >= 0 && x < M && maze[y][x] !== 5;
     const d = [[1,0], [0,1], [-1,0], [0,-1]];
-    const inside = (y, x) => y >= 0 && y < N && x >= 0 && x < M && maze[y][x] !== 5;
-    const posKey = (y, x) => `${y},${x}`;
-
-    // 초기 상태
-    const rStartSet = new Set([posKey(rsy, rsx)]);
-    const bStartSet = new Set([posKey(bsy, bsx)]);
-    const queue = [[rsy, rsx, bsy, bsx, rStartSet, bStartSet, 0]];
-
-    const seen = new Set();
-    const setToKey = (S) => [...S].sort().join('|');
-    seen.add(`${rsy},${rsx}|${bsy},${bsx}|${setToKey(rStartSet)}|${setToKey(bStartSet)}`);
-
+    
     for (let head = 0; head < queue.length; head++) {
-        const [ry, rx, by, bx, rSet, bSet, t] = queue[head];
+        const [curRX, curRY, curBX, curBY, curRSet, curBSet, turn] = queue[head];
         
-        if (ry === rgy && rx === rgx && by === bgy && bx === bgx) {
-          anw = t;
-          return anw;
+        if (curRX === targetRX && curRY === targetRY && curBX === targetBX && curBY === targetBY) {
+          return turn;
         }
 
-        const rMoves = (ry === rgy && rx === rgx)
-          ? [[ry, rx]]
-          : d.map(([dy, dx]) => [ry + dy, rx + dx])
-             .filter(([ny, nx]) => inside(ny, nx) && !rSet.has(posKey(ny, nx)));
+        const rMoves = (curRX === targetRX && curRY === targetRY)
+          ? [[curRY, curRX]]
+          : d.map(([dy, dx]) => [curRY + dy, curRX + dx])
+             .filter(([ny, nx]) => checkInside(ny, nx) && !curRSet.has(`${ny}, ${nx}`));
 
-        const bMoves = (by === bgy && bx === bgx)
-          ? [[by, bx]]
-          : d.map(([dy, dx]) => [by + dy, bx + dx])
-             .filter(([ny, nx]) => inside(ny, nx) && !bSet.has(posKey(ny, nx)));
+        const bMoves = (curBX === targetBX && curBY === targetBY)
+          ? [[curBY, curBX]]
+          : d.map(([dy, dx]) => [curBY + dy, curBX + dx])
+             .filter(([ny, nx]) => checkInside(ny, nx) && !curBSet.has(`${ny}, ${nx}`));
 
-        for (const [nry, nrx] of rMoves) {
-            for (const [nby, nbx] of bMoves) {
-                if (nry === nby && nrx === nbx) continue;
-                if (nry === by && nrx === bx && nby === ry && nbx === rx) continue;
+        for (const [nextRY, nextRX] of rMoves) {
+            for (const [nextBY, nextBX] of bMoves) {
+                // 같은 칸 도달
+                if (nextRY === nextBY && nextRX === nextBX) continue;
+                // 서로 위치만 바꾸는 경우
+                if (nextRY === curBY && nextRX === curBX && nextBY === curRY && nextBX === curRX) continue;
 
-                const newRSet = new Set(rSet);
-                const newBSet = new Set(bSet);
-                newRSet.add(posKey(nry, nrx));
-                newBSet.add(posKey(nby, nbx));
+                const nextRSet = new Set(curRSet);
+                const nextBSet = new Set(curBSet);
+                nextRSet.add(`${nextRY}, ${nextRX}`);
+                nextBSet.add(`${nextBY}, ${nextBX}`);
 
-                const key = `${nry},${nrx}|${nby},${nbx}|${setToKey(newRSet)}|${setToKey(newBSet)}`;
-                if (seen.has(key)) continue;
-                seen.add(key);
-
-                queue.push([nry, nrx, nby, nbx, newRSet, newBSet, t + 1]);
+                const key = `${nextRY},${nextRX}+${nextBY},${nextBX}+${setToKey(nextRSet)}+${setToKey(nextBSet)}`;
+                if(visited.has(key)) continue;
+                visited.add(key);
+                queue.push([nextRX, nextRY, nextBX, nextBY, nextRSet, nextBSet, turn+ 1]);
             }
         }
     }
