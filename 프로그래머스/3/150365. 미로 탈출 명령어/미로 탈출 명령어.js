32d70b27d9417ab9d4ffcac356c289df8d8ee080
MIHYUN <56717514+parkmihyunn@users.noreply.github.com>
2026-02-12 22:51:37 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.61 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -5,13 +5,13 @@ function solution(n, m, x, y, r, c, k) {
   const dist = (a, b) => Math.abs(a - tx) + Math.abs(b - ty);
 
   const d0 = Math.abs(sx - tx) + Math.abs(sy - ty);
+  
   if (d0 > k) return "impossible";
   if ((k - d0) % 2 !== 0) return "impossible";
 
   const inRange = (a, b) => a >= 0 && a < n && b >= 0 && b < m;
-
+    
   const d = [[1, 0, "d"], [0, -1, "l"], [0, 1, "r"], [-1, 0, "u"]];
-
   let anw = "";
 
   for (let step = 0; step < k; step++) {
